e3cd2da9c38ded91f9665b660c5b0dfb378b9013
gaozihao1216 <@819024636qq.com>
2026-06-04 17:24:39 +0800

    remove api place

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -4,6 +4,7 @@ export * from "./auth-api.js";
 export * from "./user-api.js";
 export * from "./designer-api.js";
 export * from "./admin-api.js";
+export * from "./ui-api.js";
 export * from "./player-api.js";
 export * from "./system-api.js";
 
